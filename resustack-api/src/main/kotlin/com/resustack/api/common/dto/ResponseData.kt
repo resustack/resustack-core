d5a91f3a6a8e356622ac72d8ff879602d3336f14
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-12-31 20:57:26 +0900

    feat: 공통 response 정의

---
+++ resustack-api/src/main/kotlin/com/resustack/api/common/dto/ResponseData.kt
@@ -0,0 +1,38 @@
+package com.resustack.api.common.dto
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ResponseData<T>(
+    val timestamp: LocalDateTime,
+    val httpStatus: Int,
+    val data: T? = null,
+    val errorCode: ErrorCode? = null
+) {
+    private constructor(
+        httpStatus: Int,
+        data: T? = null,
+        errorCode: ErrorCode? = null
+    ) : this(
+        timestamp = LocalDateTime.now(),
+        httpStatus = httpStatus,
+        data = data,
+        errorCode = errorCode
+    )
+
+    companion object {
+        fun <T> of(
+            httpStatus: HttpStatus,
+            data: T? = null,
+            errorCode: ErrorCode? = null
+        ): ResponseData<T> {
+            return ResponseData(
+                httpStatus = httpStatus.value(),
+                data = data,
+                errorCode = errorCode
+            )
+        }
+    }
+}
