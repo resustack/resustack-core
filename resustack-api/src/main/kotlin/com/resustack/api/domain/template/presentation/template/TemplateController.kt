947f57636de30acbf591d6cd2d5112dcada56369
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-01 15:21:36 +0900

    refactor: 불필요 companion 제거

--- resustack-api/src/main/kotlin/com/resustack/api/domain/template/presentation/template/TemplateController.kt
+++ resustack-api/src/main/kotlin/com/resustack/api/domain/template/presentation/template/TemplateController.kt
@@ -31,7 +31,7 @@ class TemplateController(
         @Valid @RequestBody request: TemplateCreateRequest
     ): ResponseEntity<ResponseData<TemplateResponse>> {
         val response = templateService.createTemplate(request)
-        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseData.Companion.of(HttpStatus.CREATED, response))
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseData.of(HttpStatus.CREATED, response))
     }
 
     /**
@@ -42,7 +42,7 @@ class TemplateController(
         @PathVariable id: String
     ): ResponseEntity<ResponseData<TemplateResponse>> {
         val response = templateService.getTemplateById(id)
-        return ResponseEntity.ok(ResponseData.Companion.of(HttpStatus.OK, response))
+        return ResponseEntity.ok(ResponseData.of(HttpStatus.OK, response))
     }
 
     /**
@@ -53,6 +53,6 @@ class TemplateController(
         @RequestParam status: TemplateStatus
     ): ResponseEntity<ResponseData<List<TemplateResponse>>> {
         val responses = templateService.findAllTemplatesByStatus(status)
-        return ResponseEntity.ok(ResponseData.Companion.of(HttpStatus.OK, responses))
+        return ResponseEntity.ok(ResponseData.of(HttpStatus.OK, responses))
     }
 }
