e9e63df1869d40c44efdcac5c6453964a72ff932
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-02 22:03:58 +0900

    chore: 스프링 aop 설정

--- resustack-api/src/main/kotlin/com/resustack/api/ResustackApiApplication.kt
+++ resustack-api/src/main/kotlin/com/resustack/api/ResustackApiApplication.kt
@@ -2,8 +2,10 @@ package com.resustack.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 class ResustackApiApplication
 
 fun main(args: Array<String>) {
