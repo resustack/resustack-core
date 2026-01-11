eaebb6c2b3d8d0d837f015bd5bdc54c243d3f64f
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-11 19:29:31 +0900

    feat: users 테이블 nickname to name 컬럼 네이밍 변경

--- resustack-common/src/main/resources/db/postgres/changelog/01_create_users_table.sql
+++ resustack-common/src/main/resources/db/postgres/changelog/01_create_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
                                      id                BIGSERIAL PRIMARY KEY   NOT NULL,
                                      status            TEXT                    NOT NULL,
                                      email             TEXT                    NOT NULL,
-                                     nickname          TEXT                    NOT NULL,
+                                     name              TEXT                    NOT NULL,
                                      profile_image_url TEXT                    NOT NULL,
                                      gender            TEXT                    NOT NULL,
                                      birth_year        TEXT                    NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 COMMENT ON COLUMN users.id IS '사용자 id';
 COMMENT ON COLUMN users.status IS '사용자의 status';
 COMMENT ON COLUMN users.email IS '사용자의 이메일';
-COMMENT ON COLUMN users.nickname IS '사용자의 닉네임';
+COMMENT ON COLUMN users.name IS '사용자의 이름';
 COMMENT ON COLUMN users.profile_image_url IS '사용자의 프로필 사진 URL';
 COMMENT ON COLUMN users.gender IS '사용자의 성별';
 COMMENT ON COLUMN users.birth_year IS '사용자의 출생연도';
@@ -24,4 +24,4 @@ COMMENT ON COLUMN users.created_at IS '데이터 생성일자';
 COMMENT ON COLUMN users.updated_at IS '데이터 수정일자';
 
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS uidx_users_email ON users (email);
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS uidx_users_nickname ON users (nickname);
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS uidx_users_name ON users (name);
