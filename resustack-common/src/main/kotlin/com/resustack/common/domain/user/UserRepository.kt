54dfa066489e75b4dea28d87f534450542cbcbc2
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-11 19:48:16 +0900

    feat: UserRepository 인터페이스 추가

---
+++ resustack-common/src/main/kotlin/com/resustack/common/domain/user/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.resustack.common.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(email: String): Optional<User>
+}
+
