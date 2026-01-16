b50c6acf81ecaca8dfdb8f155e78e98b0e654b43
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-16 17:12:41 +0900

    chore: 확장성을 위해 MongoDB ReadPreference/WriteConcern 정책 적용

--- resustack-api/src/main/kotlin/com/resustack/api/config/MongoConfig.kt
+++ resustack-api/src/main/kotlin/com/resustack/api/config/MongoConfig.kt
@@ -6,11 +6,27 @@ import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 
+import com.mongodb.ReadPreference
+import com.mongodb.WriteConcern
+import java.util.concurrent.TimeUnit
+import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer
+
 @Configuration
 @EnableMongoAuditing
 class MongoConfig {
     @Bean
     fun transactionManager(dbFactory: MongoDatabaseFactory): MongoTransactionManager {
         return MongoTransactionManager(dbFactory)
+    }
+
+    @Bean
+    fun mongoClientSettingsBuilderCustomizer(): MongoClientSettingsBuilderCustomizer {
+        return MongoClientSettingsBuilderCustomizer { builder ->
+            builder
+                .readPreference(ReadPreference.secondaryPreferred())
+                .writeConcern(WriteConcern.MAJORITY
+                    .withJournal(true)
+                    .withWTimeout(5, TimeUnit.SECONDS))
+        }
     }
 }
