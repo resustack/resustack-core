478807e9c46bca74938fed0a1d5ef883ae937ffd
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-01-19 15:55:30 +0900

    refactor: HTTP-Only 쿠키로 토큰 전달하도록 수정

--- resustack-auth/src/main/kotlin/com/resustack/auth/oauth/handler/OAuth2AuthenticationSuccessHandler.kt
+++ resustack-auth/src/main/kotlin/com/resustack/auth/oauth/handler/OAuth2AuthenticationSuccessHandler.kt
@@ -1,6 +1,9 @@
 package com.resustack.auth.oauth.handler
 
 import com.resustack.auth.global.security.jwt.JwtTokenGenerator
+import com.resustack.common.security.cookie.CookieProperties
+import com.resustack.common.security.cookie.CookieUtils
+import com.resustack.common.security.jwt.JwtProperties
 import com.resustack.common.security.principal.PrincipalDetails
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -10,22 +13,18 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import org.springframework.web.util.UriComponentsBuilder
 
 @Component
 class OAuth2AuthenticationSuccessHandler(
-    private val jwtTokenGenerator: JwtTokenGenerator
+    private val jwtTokenGenerator: JwtTokenGenerator,
+    private val cookieProperties: CookieProperties,
+    private val jwtProperties: JwtProperties
 ) : AuthenticationSuccessHandler {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
     @Value("\${app.oauth2.redirect-uri:http://localhost:3000/auth/callback}")
     private lateinit var redirectUri: String
-
-    companion object {
-        private const val ACCESS_TOKEN = "accessToken"
-        private const val REFRESH_TOKEN = "refreshToken"
-    }
 
     override fun onAuthenticationSuccess(
         request: HttpServletRequest,
@@ -48,15 +47,25 @@ class OAuth2AuthenticationSuccessHandler(
             authorities = authorities
         )
 
-        // TODO: HTTP-Only 쿠키로 토큰 전달하도록 수정
-        val targetUrl = UriComponentsBuilder.fromUriString(redirectUri)
-            .queryParam(ACCESS_TOKEN, tokenResponse.accessToken)
-            .queryParam(REFRESH_TOKEN, tokenResponse.refreshToken)
-            .build()
-            .toUriString()
+        // HTTP-Only 쿠키로 토큰 전달 (보안 개선)
+        val accessTokenCookie = CookieUtils.createAccessTokenCookie(
+            token = tokenResponse.accessToken,
+            maxAge = jwtProperties.accessTokenValidityInSeconds,
+            properties = cookieProperties
+        )
 
-        log.info("Redirecting to: $targetUrl")
-        response.sendRedirect(targetUrl)
+        val refreshTokenCookie = CookieUtils.createRefreshTokenCookie(
+            token = tokenResponse.refreshToken,
+            maxAge = jwtProperties.refreshTokenValidityInSeconds,
+            properties = cookieProperties
+        )
+
+        response.addHeader("Set-Cookie", accessTokenCookie.toString())
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString())
+
+        log.info("JWT tokens set as HTTP-Only cookies for user: ${user.email}")
+
+        // 쿼리 파라미터 없이 리다이렉트
+        response.sendRedirect(redirectUri)
     }
 }
-
